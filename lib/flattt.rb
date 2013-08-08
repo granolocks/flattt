7d6bfd176bb9e9305ebfbed1d2da2b57c3766654
Gabe Koss <gabe@gabekoss.com>
2013-08-07 22:03:13 -0400

    require 'yaml'

--- lib/flattt.rb
+++ lib/flattt.rb
@@ -3,6 +3,7 @@ require 'ostruct'
 require 'rdoc'
 require 'coffee-script'
 require 'sass'
+require 'yaml'
 require File.expand_path('../flattt/renderers/init.rb', __FILE__)
 require File.expand_path('../flattt/builders/init.rb', __FILE__)
 require File.expand_path('../flattt/generators/init.rb', __FILE__)
